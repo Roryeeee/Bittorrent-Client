f8ee0fd6204ab630ae1956e26a0efb7e2c6b46d5
Ayush Shrestha <ayushshresthasr71@gmail.com>
2025-02-06 08:36:34 +0545

    moving to tracker js
    writing tracker related js to another js file and importing tracker to get the list of peers

--- main.js
+++ main.js
@@ -1,52 +1,12 @@
 'use strict';
+const fs = require('fs');
+const bencode = require('bencode');
+const tracker = require('./tracker');
 
-const dgram = require('dgram');
-const Buffer = require('buffer').Buffer;
 
-module.exports.getPeers = (torrent, callback) => {
-  const socket = dgram.createSocket('udp4');
-  const url = torrent.announce.toString('utf8');
+const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
 
-  // 1. send connect request
-  udpSend(socket, buildConnReq(), url);
 
-  socket.on('message', response => {
-    if (respType(response) === 'connect') {
-      // 2. receive and parse connect response
-      const connResp = parseConnResp(response);
-      // 3. send announce request
-      const announceReq = buildAnnounceReq(connResp.connectionId);
-      udpSend(socket, announceReq, url);
-    } else if (respType(response) === 'announce') {
-      // 4. parse announce response
-      const announceResp = parseAnnounceResp(response);
-      // 5. pass peers to callback
-      callback(announceResp.peers);
-    }
-  });
-};
-
-function udpSend(socket, message, rawUrl, callback = () => {}) {
-  const url = new URL(rawUrl);
-  socket.send(message, 0, message.length, url.port, url.hostname, callback);
-}
-
-function respType(resp) {
-  // ...
-}
-
-function buildConnReq() {
-  // ...
-}
-
-function parseConnResp(resp) {
-  // ...
-}
-
-function buildAnnounceReq(connId) {
-  // ...
-}
-
-function parseAnnounceResp(resp) {
-  // ...
-}
+tracker.getPeers(torrent, peers => {
+    console.log('list of peers: ', peers);
+});
