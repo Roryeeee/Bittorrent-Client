3c7a2cf89dbea8c3906cbefa292699e826667d67
Ayush Shrestha <ayushshresthasr71@gmail.com>
2025-02-09 19:33:40 +0545

    building announce request

--- tracker.js
+++ tracker.js
@@ -64,8 +64,47 @@ function parseConnResp(resp) {
   }
 }
 
-function buildAnnounceReq(connId) {
-  // ...
+
+const torrentParser = require('./torrent-parser');
+const util = require('./util');
+
+module.exports.getPeers = (torrent, callback) => {
+  const announceReq = buildAnnounceReq(connResp.connectionId, torrent);
+}
+
+
+function buildAnnounceReq(connId, torrent, port=6881) {
+const buf = Buffer.allocUnsafe(98);
+
+
+//connection id
+connId.copy(buf,0);
+//action
+buf.writeUInt32BE(1, 8);
+//transaction id
+crypto.randomBytes(4).copy(buf, 12);
+//info hash
+torrentParser.infoHash(torrent).copy(buf, 16);
+//peerId
+util.genId().copy(buf, 36);
+//downloaded
+buffer.alloc(8).copy(buf, 56)
+//left
+torrentParser.size(torrent).copy(buf, 64);
+//uploaded
+buffer.alloc(8).copy(buf, 72);
+//event
+buf.writeUInt32BE(0, 80);
+//ip address
+buf.writeUInt32BE(0, 80);
+//key
+crypto.randomBytes(4).copy(buf, 88);
+//num want
+buf.writeInt32BE(-1, 92);
+//part
+buf.writeUInt16BE(port, 96);
+
+return buf;
 }
 
 function parseAnnounceResp(resp) {
